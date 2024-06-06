36b2bfe41635283acf46db6b6134eab53211f6ef
garold664 <garold664@gmail.com>
2024-06-06 18:58:49 +0300

    added cache function from nextjs

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -4,7 +4,7 @@ import { getMessages } from '@/lib/messages';
 
 // export const dynamic = 'force-dynamic';
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
   // unstable_noStore();
   // const response = await fetch('http://localhost:8080/messages', {
   //   next: {
@@ -13,7 +13,7 @@ export default function MessagesPage() {
   // });
   // const messages = await response.json();
 
-  const messages = getMessages();
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
